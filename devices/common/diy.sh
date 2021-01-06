42b2678f646695ac75e15ba7f307821392957654
icons <65128346+gogogojason@users.noreply.github.com>
2021-01-07 01:24:31 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -61,8 +61,6 @@ rm -f feeds/custom/luci-app-gpsysupgrade/luasrc/view/admin_status/index/links.ht
 git clone https://github.com/gogogojason/logos.git package/logos
 cp package/logos/oplinks.htm feeds/custom/luci-app-gpsysupgrade/luasrc/view/admin_status/index/links.htm
 sed -i 's/系统在线更新/系统升级/g' feeds/custom/luci-app-gpsysupgrade/po/zh_Hans/gpsysupgrade.po
-named=``
-named1=``
 sed -i "1i\msgstr \"Passwall+\"" feeds/custom/luci-app-bypass/po/zh_Hans/bypass.zh-cn.po
 sed -i "1i\msgid \"Bypass\"" feeds/custom/luci-app-bypass/po/zh_Hans/bypass.zh-cn.po
 sed -i "2a\lovertg520" feeds/custom/luci-app-bypass/po/zh_Hans/bypass.zh-cn.po
