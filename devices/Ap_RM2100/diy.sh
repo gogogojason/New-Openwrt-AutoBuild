9fe559ab9edeb12bb8a306ce5969b1d4c10d5b4d
icons <65128346+gogogojason@users.noreply.github.com>
2021-01-06 20:39:31 +0800

    Update diy.sh

--- devices/Ap_RM2100/diy.sh
+++ devices/Ap_RM2100/diy.sh
@@ -2,3 +2,7 @@ find target/linux/ramips/* -maxdepth 0 ! -path '*/patches-5.4' -exec rm -Rf '{}'
 echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
 rm -Rf target/linux/ramips/.svn
 echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/patches-5.4 target/linux/ramips/patches-5.4
+
+sed -i "s/OpenWrt_2G/RM2100/g" custom/mt-drivers/mt_wifi/files/mt7603.dat
+sed -i "s/OpenWrt_5G/RM2100_5G/g" custom/mt-drivers/mt_wifi/files/mt7615.1.5G.dat
+sed -i "s/OpenWrt_5G/RM2100_5G/g" custom/mt-drivers/mt_wifi/files/mt7615.5G.dat
