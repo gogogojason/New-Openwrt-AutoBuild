9c8e6d675f756979fa405f9d87759a1f293d5b0e
icons <65128346+gogogojason@users.noreply.github.com>
2021-01-07 01:24:18 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -61,10 +61,10 @@ rm -f feeds/custom/luci-app-gpsysupgrade/luasrc/view/admin_status/index/links.ht
 git clone https://github.com/gogogojason/logos.git package/logos
 cp package/logos/oplinks.htm feeds/custom/luci-app-gpsysupgrade/luasrc/view/admin_status/index/links.htm
 sed -i 's/系统在线更新/系统升级/g' feeds/custom/luci-app-gpsysupgrade/po/zh_Hans/gpsysupgrade.po
-named=`msgstr "Passwall+"`
-named1=`msgid "Bypass"`
-sed -i "1i\$named" feeds/custom/luci-app-bypass/po/zh_Hans/bypass.zh-cn.po
-sed -i "1i\$named1" feeds/custom/luci-app-bypass/po/zh_Hans/bypass.zh-cn.po
-sed -i "3a\lovertg520" feeds/custom/luci-app-bypass/po/zh_Hans/bypass.zh-cn.po
+named=``
+named1=``
+sed -i "1i\msgstr \"Passwall+\"" feeds/custom/luci-app-bypass/po/zh_Hans/bypass.zh-cn.po
+sed -i "1i\msgid \"Bypass\"" feeds/custom/luci-app-bypass/po/zh_Hans/bypass.zh-cn.po
+sed -i "2a\lovertg520" feeds/custom/luci-app-bypass/po/zh_Hans/bypass.zh-cn.po
 sed -i 's/lovertg520//g' feeds/custom/luci-app-bypass/po/zh_Hans/bypass.zh-cn.po
 sed -i 's/"IP限速"/"网速控制"/g' feeds/custom/luci-app-eqos/po/zh_Hans/eqos.po
