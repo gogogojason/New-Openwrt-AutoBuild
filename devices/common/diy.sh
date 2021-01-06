42858e856795a3d33c984da87d4fddf2b32c102a
icons <65128346+gogogojason@users.noreply.github.com>
2021-01-07 01:45:39 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -53,6 +53,7 @@ date=`date +%m.%d.%Y`
 date1=`date +%Y.%m.%d`
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='hfy166 Ver.$date1'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/OpenWrt/MiRouter/g" package/base-files/files/bin/config_generate
+sed -i "s/192.168.1.1/192.168.2.1/g" package/base-files/files/bin/config_generate
 sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
 sed -i '$a cgi-timeout = 300' package/feeds/packages/uwsgi/files-luci-support/luci-webui.ini
 sed -i 's/https:\/\/op.supes.top/http:\/\/openwrt.ink:8666/g' feeds/custom/luci-app-gpsysupgrade/root/usr/bin/upgrade.lua
