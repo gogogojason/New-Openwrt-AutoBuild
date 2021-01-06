c4a32f4a4209beb05410a27a03822c8cd891e8bc
icons <65128346+gogogojason@users.noreply.github.com>
2021-01-07 00:14:33 +0800

    Update diy.sh

--- devices/Ap_RM2100/diy.sh
+++ devices/Ap_RM2100/diy.sh
@@ -3,11 +3,10 @@ echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ra
 rm -Rf target/linux/ramips/.svn
 echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/patches-5.4 target/linux/ramips/patches-5.4
 
-sed -i "s/OpenWrt_2G/RM2100/g" custom/mt-drivers/mt_wifi/files/mt7603.dat
-sed -i "s/OpenWrt_5G/RM2100_5G/g" custom/mt-drivers/mt_wifi/files/mt7615.1.5G.dat
-sed -i "s/OpenWrt_5G/RM2100_5G/g" custom/mt-drivers/mt_wifi/files/mt7615.5G.dat
+sed -i "s/OpenWrt_2G/RM2100/g" feeds/custom/mt-drivers/mt_wifi/files/mt7603.dat
+sed -i "s/OpenWrt_5G/RM2100_5G/g" feeds/custom/mt-drivers/mt_wifi/files/mt7615.1.5G.dat
+sed -i "s/OpenWrt_5G/RM2100_5G/g" feeds/custom/mt-drivers/mt_wifi/files/mt7615.5G.dat
 
-git clone https://github.com/gogogojason/logos.git custom/logos
-rm -f custom/luci-theme-edge/htdocs/luci-static/edge/logo.png
-cp custom/logos/milogo.png custom/luci-theme-edge/htdocs/luci-static/edge/logo.png
-rm custom/logos -r
+rm -f feeds/custom/luci-theme-edge/htdocs/luci-static/edge/logo.png
+cp package/logos/milogo.png feeds/custom/luci-theme-edge/htdocs/luci-static/edge/logo.png
+rm package/logos -r
