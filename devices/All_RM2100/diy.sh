54e75cfc59a9057740d279315994fae3db1d68f7
icons <65128346+gogogojason@users.noreply.github.com>
2021-01-07 19:56:56 +0800

    Update diy.sh

--- devices/All_RM2100/diy.sh
+++ devices/All_RM2100/diy.sh
@@ -4,8 +4,9 @@ rm -Rf target/linux/ramips/.svn
 echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/patches-5.4 target/linux/ramips/patches-5.4
 
 sed -i "s/OpenWrt_2G/RMWiFi/g" feeds/custom/mt-drivers/mt_wifi/files/mt7603.dat
-sed -i "s/OpenWrt_5G/RMWiFi_5G/g" feeds/custom/mt-drivers/mt_wifi/files/mt7615.1.5G.dat
-sed -i "s/OpenWrt_5G/RMWiFi_5G/g" feeds/custom/mt-drivers/mt_wifi/files/mt7615.5G.dat
+sed -i "s/OpenWrt_5G/RMWiFi_5G/g" feeds/custom/mt-drivers/mt_wifi/files/mt7612.dat
+sed -i "s/OpenWrt_5G/RMWiFi_5G/g" feeds/custom/mt-drivers/mt_wifi/files/mt7615.dat
+
 
 sed -i 's/${Arch}/${Arch}_softfloat/g' feeds/custom/luci-app-adguardhome/root/usr/share/AdGuardHome/links.txt
 rm -f feeds/custom/luci-theme-edge/htdocs/luci-static/edge/logo.png
