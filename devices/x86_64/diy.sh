ed5e858a5621acfff52d8571e99ebd184e1d9d9b
icons <65128346+gogogojason@users.noreply.github.com>
2021-01-10 18:28:39 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sed -i "s/OpenWrt/MyRouter/g" package/base-files/files/bin/config_generate
+rm -f feeds/custom/luci-app-unblockmusic/po/zh_Hans/unblockmusic.po
+cp package/logos/opunblockmusic.po feeds/custom/luci-app-unblockmusic/po/zh_Hans/unblockmusic.po
+rm package/logos -r
 
 echo '
 CONFIG_CRYPTO_CHACHA20_X86_64=y
