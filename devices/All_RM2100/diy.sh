769abc88ddf8bd63ea474781cea91fb2d76cdb1f
icons <65128346+gogogojason@users.noreply.github.com>
2021-01-10 16:58:46 +0800

    Update diy.sh

--- devices/All_RM2100/diy.sh
+++ devices/All_RM2100/diy.sh
@@ -10,5 +10,7 @@ sed -i "s/OpenWrt/MiRouter/g" package/base-files/files/bin/config_generate
 
 sed -i 's/${Arch}/${Arch}_softfloat/g' feeds/custom/luci-app-adguardhome/root/usr/share/AdGuardHome/links.txt
 rm -f feeds/custom/luci-theme-edge/htdocs/luci-static/edge/logo.png
+rm -f feeds/custom/luci-app-unblockmusic/po/zh_Hans/unblockmusic.po
 cp package/logos/milogo.png feeds/custom/luci-theme-edge/htdocs/luci-static/edge/logo.png
+cp package/logos/opunblockmusic.po feeds/custom/luci-app-unblockmusic/po/zh_Hans/unblockmusic.po
 rm package/logos -r
