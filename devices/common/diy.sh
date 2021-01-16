eae3403d7ee5327cd7346d8ef5086e3dd4bf64a5
icons <65128346+gogogojason@users.noreply.github.com>
2021-01-16 16:45:48 +0800

    Update diy.sh

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -67,7 +67,7 @@ rm -f feeds/custom/luci-app-gpsysupgrade/luasrc/view/admin_status/index/links.ht
 git clone https://github.com/gogogojason/logos.git package/logos
 cp package/logos/oplinks.htm feeds/custom/luci-app-gpsysupgrade/luasrc/view/admin_status/index/links.htm
 sed -i 's/系统在线更新/系统升级/g' feeds/custom/luci-app-gpsysupgrade/po/zh_Hans/gpsysupgrade.po
-sed -i "1i\msgstr \"Passwall+\"" feeds/custom/luci-app-bypass/po/zh_Hans/bypass.zh-cn.po
+sed -i "1i\msgstr \"PassWall+\"" feeds/custom/luci-app-bypass/po/zh_Hans/bypass.zh-cn.po
 sed -i "1i\msgid \"Bypass\"" feeds/custom/luci-app-bypass/po/zh_Hans/bypass.zh-cn.po
 sed -i "2a\lovertg520" feeds/custom/luci-app-bypass/po/zh_Hans/bypass.zh-cn.po
 sed -i 's/lovertg520//g' feeds/custom/luci-app-bypass/po/zh_Hans/bypass.zh-cn.po
