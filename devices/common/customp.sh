b72e4e5d430bef9bab2e1854eb5ee480d5fc1186
icons <65128346+gogogojason@users.noreply.github.com>
2021-01-21 00:35:00 +0800

    Delete customp.sh

--- devices/common/customp.sh
+++
@@ -1,5 +0,0 @@
-sed -i "/mediaurlbase/d" package/*/*/luci-theme*/root/etc/uci-defaults/*
-
-sed -i '/docker/d' package/feeds/custom/luci-app-dockerman/Makefile
-
-mv -f feeds/packages/net/nlbwmon package/feeds/custom/nlbwmon
