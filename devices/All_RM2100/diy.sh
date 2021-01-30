a8933d649c8f06d48ba692f6b5fad8d2bfd12244
icons <65128346+gogogojason@users.noreply.github.com>
2021-01-30 19:29:02 +0800

    Update diy.sh

--- devices/All_RM2100/diy.sh
+++ devices/All_RM2100/diy.sh
@@ -14,4 +14,5 @@ rm -f feeds/custom/luci-theme-edge/htdocs/luci-static/edge/logo.png
 rm -f feeds/custom/luci-app-unblockmusic/po/zh_Hans/unblockmusic.po
 cp package/logos/milogo.png feeds/custom/luci-theme-edge/htdocs/luci-static/edge/logo.png
 cp package/logos/opunblockmusic.po feeds/custom/luci-app-unblockmusic/po/zh_Hans/unblockmusic.po
+cp package/logos/opcpuinfo files/sbin/cpuinfo
 rm -Rf package/logos
